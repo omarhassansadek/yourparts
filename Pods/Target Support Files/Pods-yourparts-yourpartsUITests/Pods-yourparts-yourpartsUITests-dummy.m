7ae08ba9f02a3bc818b06a7a7f651c8ce12e1326
omar1taha <omar.taha_94@hotmail.com>
2020-07-08 07:53:29 +0200

    removepods

--- Pods/Target Support Files/Pods-yourparts-yourpartsUITests/Pods-yourparts-yourpartsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_yourparts_yourpartsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_yourparts_yourpartsUITests
-@end
--- Pods/Target Support Files/Pods-yourpartsTests/Pods-yourpartsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_yourpartsTests : NSObject
-@end
-@implementation PodsDummy_Pods_yourpartsTests
-@end
