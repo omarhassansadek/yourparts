85bcc092867542ebbdd0f6c514c129288e28d17c
Mostafa-Rayan <mostafarabe7@gmail.com>
2020-11-03 13:35:40 +0200

    .

--- yourparts/Modules/Spare Parts/detailSpartViewController.swift
+++ yourparts/Modules/Spare Parts/detailSpartViewController.swift
@@ -9,29 +9,32 @@
 import UIKit
 
 class detailSpartViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
-
+    
+    //MARK:- Outlets
     @IBOutlet weak var detailSpartTableView: UITableView!
+    
+    //MARK:- Variables
     var spartName: String?
     
+    //MARK:- Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-            
         
         if let topItem = self.navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         }
         
-        
         self.navigationController?.navigationBar.tintColor = primaryColor
         
         let nib = UINib(nibName: String(describing: headerTableViewCell.self), bundle: nil)
         self.detailSpartTableView.register(nib, forCellReuseIdentifier: "headerCell")
-
+        
         self.detailSpartTableView.dataSource = self
         self.detailSpartTableView.delegate = self
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    //MARK:- TableView Delegates
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -42,25 +45,14 @@ class detailSpartViewController: UIViewController , UITableViewDelegate, UITable
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-    
-            let cell = tableView.dequeueReusableCell(withIdentifier: "headerCell") as! headerTableViewCell
-            cell.cellTitle.text = spartName ?? "العفشة"
-            cell.cellTitle.textColor = UIColor.white
-            cell.spartsBg.isHidden = false
-            cell.searchTf.layer.opacity = 1.0
-            
-            return cell
-
-        }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "headerCell") as! headerTableViewCell
+        cell.cellTitle.text = spartName ?? "العفشة"
+        cell.cellTitle.textColor = UIColor.white
+        cell.spartsBg.isHidden = false
+        cell.searchTf.layer.opacity = 1.0
+        
+        return cell
+        
     }
-    */
-
+    
 }
--- yourparts/Modules/Spare Parts/spartsCollectionDelegate.swift
+++ yourparts/Modules/Spare Parts/spartsCollectionDelegate.swift
@@ -8,40 +8,37 @@
 
 import Foundation
 import UIKit
-//import UICollectionViewLeftAlignedLayout
 
 class spartsCollectionDelegate : NSObject ,UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-
+    
+    //MARK:- Variables
     var targetController : newHomeViewController?
     var row: Int?
     var type: String?
     
+    //MARK:- CollectionView Delegates
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 6
-        
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-            let cellCollection = collectionView.dequeueReusableCell(withReuseIdentifier: "productCollection", for: indexPath) as! productCollectionViewCell
-            
-            cellCollection.productimg.image = UIImage(named:"teelFramel")
-            
-            return cellCollection
+        let cellCollection = collectionView.dequeueReusableCell(withReuseIdentifier: "productCollection", for: indexPath) as! productCollectionViewCell
+        cellCollection.productimg.image = UIImage(named:"teelFramel")
+        
+        return cellCollection
         
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if indexPath.row == 5{
-
+            
         }
     }
-    
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0.0
@@ -51,13 +48,9 @@ class spartsCollectionDelegate : NSObject ,UICollectionViewDataSource, UICollect
         return 0.0
     }
     
-    
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         print((self.targetController?.view.frame.width ?? 0.0)  - 50.0)
         return CGSize(width: (  (self.targetController?.view.frame.width ?? 0.0)  - 50.0 ), height: collectionView.frame.height )
     }
-    
-   
     
 }
